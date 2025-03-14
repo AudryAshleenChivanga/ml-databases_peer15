1ad5173c859620b446cc6f2d6c03f476e60f49b7
Ngum Dieudonne <127442228+Ngum12@users.noreply.github.com>
2025-03-14 07:09:58 +0200

    Add files via upload

---
+++ Prediction/sample_inputs.py
@@ -0,0 +1,32 @@
+SAMPLE_INPUTS = [
+    {
+        "title": "Healthy Individual",
+        "data": {
+            'Age': 45,
+            'gender_numeric': 1,  # Male
+            'total_bilirubin': 0.8,
+            'direct_bilirubin': 0.2,
+            'alkaline_phosphotase': 150,
+            'alamine_aminotransferase': 30,
+            'aspartate_aminotransferase': 25,
+            'total_proteins': 7.5,
+            'albumin': 4.2,
+            'albumin_and_globulin_ratio': 1.8
+        }
+    },
+    {
+        "title": "Likely Liver Disease",
+        "data": {
+            'Age': 62,
+            'gender_numeric': 1,  # Male
+            'total_bilirubin': 10.9,
+            'direct_bilirubin': 5.5,
+            'alkaline_phosphotase': 699,
+            'alamine_aminotransferase': 64,
+            'aspartate_aminotransferase': 100,
+            'total_proteins': 7.5,
+            'albumin': 3.2,
+            'albumin_and_globulin_ratio': 0.74
+        }
+    }
+]
